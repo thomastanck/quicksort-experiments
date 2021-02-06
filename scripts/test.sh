a610119ebf1f754ae3b6b5e574de0494324fd470
Thomas Tan <829070+thomastanck@users.noreply.github.com>
2021-02-07 00:42:00 +0800

    test.sh recond perf counters properly

--- scripts/test.sh
+++ scripts/test.sh
@@ -42,38 +42,46 @@ dataset_sizes="100000 1000"
 sorter_ids=${sorter_ids:-$(seq 0 4)}
 
 : test str datasets
-echo dataset_seed, dataset_id, dataset_size, sorter_id, num_repeats, milliseconds, cpu, ram > $str_stats_filename
+echo dataset_seed, dataset_id, dataset_size, sorter_id, num_repeats, milliseconds, move_count, swap_count, comp_count, cpu, ram > $str_stats_filename
 for dataset_id in $str_datasets; do
     for dataset_size in $dataset_sizes; do
         python gen_data.py --seed=$dataset_seed --dataset=$dataset_id --size=$dataset_size --type=str > dataset
         for sorter_id in $sorter_ids; do
             num_repeats=$((5000000 / $dataset_size))
-            milliseconds=$(./sorting_benchmark string $sorter_id $num_repeats < dataset)
+            benchmark_info=$(./sorting_benchmark string $sorter_id $num_repeats < dataset)
+            milliseconds=$(echo $benchmark_info | cut -f1 -d' ')
+            move_count=$(echo $benchmark_info | cut -f2 -d' ')
+            swap_count=$(echo $benchmark_info | cut -f3 -d' ')
+            comp_count=$(echo $benchmark_info | cut -f4 -d' ')
             CPU=`top -b -n1 | grep "Cpu(s)" | awk '{print $2 + $4}'`
             FREE_DATA=`free -m | grep Mem` 
             CURRENT=`echo $FREE_DATA | cut -f3 -d' '`
             TOTAL=`echo $FREE_DATA | cut -f2 -d' '`
             RAM=$(echo "scale = 2; $CURRENT/$TOTAL*100" | bc)
-            echo $dataset_seed, $dataset_id, $dataset_size, $sorter_id, $num_repeats, $milliseconds, $CPU, $RAM >> $str_stats_filename
+            echo $dataset_seed, $dataset_id, $dataset_size, $sorter_id, $num_repeats, $milliseconds, $move_count, $swap_count, $comp_count, $CPU, $RAM >> $str_stats_filename
         done
     done
 done
 
 : test int datasets
-echo dataset_seed, dataset_id, dataset_size, sorter_id, num_repeats, milliseconds, cpu, ram > $int_stats_filename
+echo dataset_seed, dataset_id, dataset_size, sorter_id, num_repeats, milliseconds, move_count, swap_count, comp_count, cpu, ram > $int_stats_filename
 
 for dataset_id in $int_datasets; do
     for dataset_size in $dataset_sizes; do
         python gen_data.py --seed=$dataset_seed --dataset=$dataset_id --size=$dataset_size --type=int > dataset
         for sorter_id in $sorter_ids; do
             num_repeats=$((5000000 / $dataset_size))
-            milliseconds=$(./sorting_benchmark int $sorter_id $num_repeats < dataset)
+            benchmark_info=$(./sorting_benchmark string $sorter_id $num_repeats < dataset)
+            milliseconds=$(echo $benchmark_info | cut -f1 -d' ')
+            move_count=$(echo $benchmark_info | cut -f2 -d' ')
+            swap_count=$(echo $benchmark_info | cut -f3 -d' ')
+            comp_count=$(echo $benchmark_info | cut -f4 -d' ')
             CPU=`top -b -n1 | grep "Cpu(s)" | awk '{print $2 + $4}'`
             FREE_DATA=`free -m | grep Mem` 
             CURRENT=`echo $FREE_DATA | cut -f3 -d' '`
             TOTAL=`echo $FREE_DATA | cut -f2 -d' '`
             RAM=$(echo "scale = 2; $CURRENT/$TOTAL*100" | bc)
-            echo $dataset_seed, $dataset_id, $dataset_size, $sorter_id, $num_repeats, $milliseconds, $CPU, $RAM >> $int_stats_filename
+            echo $dataset_seed, $dataset_id, $dataset_size, $sorter_id, $num_repeats, $milliseconds, $move_count, $swap_count, $comp_count, $CPU, $RAM >> $int_stats_filename
         done
     done
 done
